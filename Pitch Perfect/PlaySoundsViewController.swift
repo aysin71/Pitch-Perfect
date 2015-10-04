299b9fa1b53cd2cfe6e844e2ccae21699ee614e1
Aysin Kuran <aysinkuran@p20030084ae7b8913c0cd8697fd82a337.dip0.t-ipconnect.de>
2015-10-04 16:55:30 +0200

    with Reverb and Echo added to the function

--- Pitch Perfect/PlaySoundsViewController.swift
+++ Pitch Perfect/PlaySoundsViewController.swift
@@ -43,26 +43,36 @@ class PlaySoundsViewController: UIViewController {
     
 
     @IBAction func playSlowAudio(sender: UIButton) {
-        playRecordedAudio(0.5, pitch: 0)
+        playRecordedAudio(0.5, pitch: 0, echo: 0, reverb: 0)
 
     }
     
     @IBAction func playFastAudion(sender: UIButton) {
-        playRecordedAudio(2, pitch: 0)
+        playRecordedAudio(2, pitch: 0, echo: 0, reverb: 0)
 
     }
     
     @IBAction func playChipmunkAudio(sender: UIButton) {
-        playRecordedAudio(1, pitch: 1000)
+        playRecordedAudio(1, pitch: 1000, echo: 0, reverb: 0)
 
     }
     
     @IBAction func playDarthvaderAudio(sender: UIButton) {
-        playRecordedAudio(1, pitch: -1000)
+        playRecordedAudio(1, pitch: -1000, echo: 0, reverb: 0)
 
     }
     
-    func playRecordedAudio(rate: Float,  pitch: Float)
+    
+    @IBAction func playEchoAudio(sender: UIButton) {
+        playRecordedAudio(1, pitch: 0, echo: 30, reverb: 0)
+    }
+    
+    @IBAction func playReverbAudio(sender: UIButton) {
+        playRecordedAudio(1, pitch: 0, echo: 0, reverb: 100)
+    }
+    
+    
+    func playRecordedAudio(rate: Float,  pitch: Float, echo: Float, reverb: Float)
     {
         audioPlayer.stop()
         audioEngine.stop()
@@ -81,17 +91,29 @@ class PlaySoundsViewController: UIViewController {
         changePitchEffect.pitch = pitch
         audioEngine.attachNode(changePitchEffect)
         
+        // Set Echo
+        
+        let changeEchoEffect = AVAudioUnitDelay()
+        changeEchoEffect.delayTime = NSTimeInterval(echo)
+        audioEngine.attachNode(changeEchoEffect)
+        
+        //Setting the reverb effect
+        let changeReverbEffect = AVAudioUnitReverb()
+        changeReverbEffect.loadFactoryPreset(AVAudioUnitReverbPreset.LargeHall2)
+        changeReverbEffect.wetDryMix = reverb
+        audioEngine.attachNode(changeReverbEffect)
+        
         //connecting effects
         audioEngine.connect(audioPlayerNode, to: changeRateEffect, format: nil)
         audioEngine.connect(changeRateEffect, to: changePitchEffect, format: nil)
-        audioEngine.connect(changePitchEffect, to: audioEngine.outputNode, format: nil)
+        audioEngine.connect(changePitchEffect, to: changeReverbEffect, format: nil)
+        audioEngine.connect(changeReverbEffect, to: changeEchoEffect, format: nil)
+        audioEngine.connect(changeEchoEffect, to: audioEngine.outputNode, format: nil)
         
+        //setting the file to be played to the recorded file
         audioPlayerNode.scheduleFile(audioFile, atTime: nil, completionHandler: nil)
         try! audioEngine.start()
         
-        print(rate)
-        print(pitch)
-
         //play the Recorded File
         audioPlayerNode.play()
 
